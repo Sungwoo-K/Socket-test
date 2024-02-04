d3d151b5434545782147bf389769be50747b01c8
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2024-02-04 17:29:47 +0900

    Add: Socket.io connect and disconnect

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Socket Test
 
----
-
 ##### Socket test를 통해 양방향 통신과 chatting service 에 대해 알아본다.
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
-import path from "path";
+import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -12,6 +14,13 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "assets/index.html"));
 });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
   console.log("Start server for Socket testing");
 });
